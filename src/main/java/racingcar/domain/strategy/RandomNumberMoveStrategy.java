edb884266d1ddf334ae1fe1b1c5a363de99c00ef
limwoobin <drogba02@naver.com>
2022-04-23 21:48:01 +0900

    refactor: 자동차의 이동전략을 인터페이스를 이용해 전략 패턴으로 구현하라
    - 현재는 랜덤값을 이용해 이동하는 전략이지만 이동전략은 변경될 여지가 있기때문에 변경에 유연한 전략 패턴을 이용한다

--- src/main/java/racingcar/domain/strategy/RandomNumberMoveStrategy.java
+++ src/main/java/racingcar/domain/strategy/RandomNumberMoveStrategy.java
@@ -1,14 +1,17 @@
-package racingcar.domain;
+package racingcar.domain.strategy;
 
-public class MoveBehavior {
+import racingcar.domain.RandomNumber;
+
+public class RandomNumberMoveStrategy implements MoveStrategy {
     private final RandomNumber randomNumber;
 
     private static final int MOVABLE_STANDARD_VALUE = 4;
 
-    public MoveBehavior() {
+    public RandomNumberMoveStrategy() {
         this.randomNumber = new RandomNumber();
     }
 
+    @Override
     public boolean isMovable() {
         return randomNumber.getRandomNumber() >= MOVABLE_STANDARD_VALUE;
     }
